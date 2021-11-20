8c9812bbbcca94bd596ed0c73f78fd6d28b702b8
PlatinaAD <93940920+PlatinaAD@users.noreply.github.com>
2021-11-20 17:40:40 +0300

    Adding Files

---
+++ index.js
@@ -0,0 +1,44 @@
+// 1 exercise
+
+// const arr = [2, 3, 4, 5];
+// let result = 1;
+
+// for (let i = 0; i < arr.length; i++) {
+//     result *= arr[i];
+// }
+
+// console.log(result);
+
+// 2 exercise
+
+// const arr = [1, 2, 5, 9, 4, 13, 4, 10];
+
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] === 4) {
+//         console.log('Есть!');
+//     }
+// }
+
+// 3 exercise
+
+// const arr = [12, 15, 20, 25, 59, 79];
+// let res = 0;
+
+// for (let i = 0; i < arr.length; i++) {
+//     res += arr[i] / arr.length;
+// }
+
+// console.log(res);
+
+// 4 exercise
+
+// const arr = [12, 15, 20, 25, 59, 79];
+// let res = 0;
+
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] % 2) {
+//         console.log(arr[i] + ' odd ');
+//     } else {
+//         console.log(arr[i] + ' even ');
+//     }
+// }
